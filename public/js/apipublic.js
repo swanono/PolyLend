32d61963e9dd6f04e236076906d4c7165d94cc61
E154706J <ulysse.guyon@etu.univ-nantes.fr>
2019-05-28 15:36:37 +0200

    ajout console.log

--- public/js/apipublic.js
+++ public/js/apipublic.js
@@ -1,5 +1,5 @@
 'use strict';
 
-
+console.log(window.location.pathname);
 document.getElementById('form_login').setAttribute('action', (window.location.pathname.includes('public') ? '../' : '') + './api/utilisateur/login/');
 document.getElementById('form_register').setAttribute('action', (window.location.pathname.includes('public') ? '../' : '') + './api/utilisateur/register/');
