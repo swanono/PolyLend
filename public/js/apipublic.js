14454140e08eb908276b7380eeab33664b04bcd4
Ulysse Guyon <ulysse.guyon@etu.univ-nantes.fr>
2019-05-28 15:34:44 +0200

    je suis con

--- public/js/apipublic.js
+++ public/js/apipublic.js
@@ -1,5 +1,4 @@
 'use strict';
 
-
 document.getElementById('form_login').setAttribute('action', (window.location.pathname.includes('public') ? '../' : '') + './api/utilisateur/login/');
 document.getElementById('form_register').setAttribute('action', (window.location.pathname.includes('public') ? '../' : '') + './api/utilisateur/register/');
